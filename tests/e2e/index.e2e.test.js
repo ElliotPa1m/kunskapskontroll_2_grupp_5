5b9af7ba9a684c72c73a7ed2bca6fcd04c0841ca
ElliotPa1m <elliot.palm@hotmail.com>
2026-04-08 10:34:36 +0200

    fixed a bug in the e2e code

--- tests/e2e/index.e2e.test.js
+++ tests/e2e/index.e2e.test.js
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("index page loads images", async ({ page }) => {
-  await page.goto("http://127.0.0.1:5500/index.html");
+  await page.goto("/");
 
   const img = page.locator("#moving-worker-img");
 
